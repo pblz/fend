dbf95b5e07a95ea80575c84f405e705dc27f3e95
Pia Balz <pia.balz@daimler.com>
2021-02-10 15:36:16 +0100

    feat: Added basic dynamic behavior - Adapt HTML to include 4 Sections - Dynamically populate nav menu via app.js - Include links to sections - Add eventListener for Scrolling / highlighting menu accordingly

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,6 +18,8 @@
  * 
 */
 
+// List to keep all sections
+let sections = [];
 
 /**
  * End Global Variables
@@ -25,7 +27,11 @@
  * 
 */
 
+// Find Sections
+function getSections() {
+    sections = document.querySelectorAll("[data-nav]");
 
+}
 
 /**
  * End Helper Functions
@@ -33,14 +39,65 @@
  * 
 */
 
-// build the nav
-
+// Build the nav
+function populateNav() {
+    // Iterate over html to get all sections: a section always has the attribute data-nav
+    sections = document.querySelectorAll("[data-nav]");
+    // Sections should go into the navbar__list
+    var navList = document.getElementById("navbar__list");
+    for (const section of sections) {
+        const newLi = document.createElement("li");
+        //newLi.innerHTML = section.dataset.nav;
+        var linkElement = document.createElement("a");
+        linkElement.href = "#"+ section.id;
+        linkElement.innerHTML=section.dataset.nav;
+        newLi.appendChild(linkElement);
+        console.log(newLi);
+        navList.appendChild(newLi);
+        if (section.classList.contains("your-active-class")){
+            newLi.classList.add("active");
+        };
+        console.log(newLi);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
 
+function checkActiveSection() {
+    console.log(window.pageYOffset);
+    sections = document.querySelectorAll("[data-nav]");
+    for (const section of sections) {
+        var rect = section.getBoundingClientRect();
+        console.log("Section " + section.id);
+        console.log(rect);
+        if (rect.top > -500) {
+            console.log(section.id + " is active");
+            var navList = document.getElementById("navbar__list");
+            var liList = navList.getElementsByTagName("li");
+            var oldActiveElement = navList.getElementsByClassName("active")[0];
+            if (oldActiveElement){
+                oldActiveElement.classList.remove("active");
+            }
+            for (liEl of liList){
+                    var s1 = liEl.textContent.toString();
+                    var s2 = section.dataset.nav.toString();
+                    if(s1 === s2){
+                        liEl.classList.add("active");
+                    }
+            }
+            break;
+        }
+    }
+
+}
+
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollTo(){
+    // Using relative links in the navElements for navigation
+    // This method just makes sure after a click, the highlight is updated if necessary
+    checkActiveSection();
+}
 
 /**
  * End Main Functions
@@ -50,8 +107,15 @@
 
 // Build menu 
 
+document.addEventListener("DOMContentLoaded", populateNav);
+
 // Scroll to section on link click
+document.getElementById("navbar__list").addEventListener("click", scrollTo);
+
 
 // Set sections as active
+document.addEventListener("scroll", checkActiveSection);
+
+
 
 
